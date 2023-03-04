975223fb40a9cc5695757f3f9859f50b0c5037d4
PortilizenHub <matthewallanvincent09@gmail.com>
2023-03-04 10:32:34 -0600

    wrong fields

--- coin_collecter/source/Main.hx
+++ coin_collecter/source/Main.hx
@@ -33,7 +33,7 @@ class PlayState extends FlxState
 		coins = new FlxGroup();
 		for (i in 0...20)
 		{
-			var coin = new FlxSprite(FlxRandom.int(0, FlxG.width - 16), FlxRandom.int(0, FlxG.height - 16));
+			var coin = new FlxSprite(FlxRandom.float(0, FlxG.width - 16), FlxRandom.float(0, FlxG.height - 16));
 			coin.makeGraphic(16, 16, 0xffffff00);
 			coins.add(coin);
 		}
@@ -43,10 +43,12 @@ class PlayState extends FlxState
 
 		score = new FlxText(4, 4, FlxG.width - 8, "");
 		score.color = 0xffffffff;
+		score.setFormat(null, 16, 0xffffffff, "center");
 		score.shadow = 0xff000000;
 
 		timer = new FlxText(4, FlxG.height - 16, FlxG.width - 8, "");
 		timer.color = 0xffffffff;
+		timer.setFormat(null, 16, 0xffffffff, "center");
 		timer.shadow = 0xff000000;
 
 		add(coins);
@@ -68,39 +70,44 @@ class PlayState extends FlxState
 			FlxG.switchState(new WinState());
 		}
 
-		timeLeft -= Math.ceil(elapsed);
+		timeLeft -= 1;
+		timer.text = "Time Left: ${timeLeft}";
+
 		if (timeLeft <= 0)
 		{
 			FlxG.switchState(new LoseState());
 		}
-
-		score.text = "Score: " + scoreCount + " / " + targetScore;
-		timer.text = "Time: " + timeLeft;
 	}
 
 	private function collectCoin(player:FlxSprite, coin:FlxSprite):Void
 	{
-		coin.kill();
-		scoreCount++;
+		coins.remove(coin, true);
+		scoreCount += 1;
+		score.text = "Score: ${scoreCount} / ${targetScore}";
 	}
 }
 
 class WinState extends FlxState
 {
-	override public function create():Void
-	{
-		var text = new FlxText(0, FlxG.height / 2 - 16, FlxG.width, "You win!");
-		text.alignment = "center";
-		add(text);
-	}
-}
+	private var winText:FlxText;
 
-class LoseState extends FlxState
-{
 	override public function create():Void
 	{
-		var text = new FlxText(0, FlxG.height / 2 - 16, FlxG.width, "You lose!");
-		text.alignment = "center";
-		add(text);
+		winText = new FlxText(0, FlxG.height / 2 - 16, FlxG.width, "You win!");
+		winText.color = 0xffffffff;
+		winText.setFormat(null, 32, 0xffffffff, "center");
+		winText.shadow = 0xff000000;
+
+		add(winText);
+	}
+
+	override public function update(elapsed:Float):Void
+	{
+		super.update(elapsed);
+
+		if (FlxG.mouse.justPressed())
+		{
+			FlxG.switchState(new PlayState());
+		}
 	}
 }
